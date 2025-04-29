66bcfd77390241864bb69e4616474e21d61e5d1e
guzmannunezj <guzmannunezj@stearns.mathcs.carleton.edu>
2025-04-29 18:52:48 -0400

    Fixed the data! And created/updated the simple CREATE TABLE functions.

--- data/database-schema.sql
+++ data/database-schema.sql
@@ -1,1 +1,49 @@
-hi
+CREATE TABLE animals (
+	id integer NOT NULL,
+	animal_name text, 
+	animal_species text, 
+	animal_lifespan text
+);
+
+CREATE TABLE continents (
+        id integer NOT NULL, 
+        continent_name text
+);
+
+CREATE TABLE countries (
+        id integer NOT NULL,
+        country_name text
+); 
+
+CREATE TABLE populationtTrend (
+        id integer NOT NULL,
+        trend text
+);
+
+CREATE TABLE populationStatus (
+        id integer NOT NULL,
+        status text
+);
+
+CREATE TABLE continents_countries (
+        continent_id integer,
+        country_id integer
+);
+
+CREATE TABLE animals_continents (
+        animal_id integer,
+	continent_id integer
+);
+
+CREATE TABLE animals_countries (
+        animal_id integer,
+        country_id integer
+);
+
+CREATE TABLE animals_concern (
+        animal_id integer,
+        trend_id integer,
+	status_id integer
+);
+
+
